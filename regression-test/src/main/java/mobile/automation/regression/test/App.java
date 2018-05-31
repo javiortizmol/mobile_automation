a391eabc43aa4ea273b92400dcd72c7f106751c9
Javier Ortiz <jahor2@gmail.com>
2018-05-31 11:51:04 -0500

    Implementacion base de mobile automation

---
+++ regression-test/src/main/java/mobile/automation/regression/test/App.java
@@ -0,0 +1,13 @@
+package mobile.automation.regression.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
